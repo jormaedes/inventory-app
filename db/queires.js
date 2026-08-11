b8faa4fe591d0fa45ae4d0eaba84ba76fce96fb0
Jormaedes Luís <jormahed@gmail.com>
2026-08-11 18:59:02 +0100

    fead: upgrade query to take data of products

--- db/queires.js
+++ db/queires.js
@@ -7,7 +7,7 @@ async function getAllCategory() {
 }
 
 async function getAllProduct() {
-	const query = "SELECT * FROM product";
+	const query = "SELECT product.id, product.name, qtd_stock, price, category.name as category_name FROM product JOIN category ON product.category_id = category.id";
 	const { rows } = await pool.query(query);
 	return rows;
 }
