c7549bae110967c2d4503df4c1ba9331c68ae4e3
Jormaedes Luís <jormahed@gmail.com>
2026-08-11 19:31:57 +0100

    feat: implement view

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import process from 'process';
 import path from 'path';
-import { getAllCategory } from './db/queires.js';
+import { getAllCategory, getAllProduct, getSingleProduct, getSingleCategory } from './db/queires.js';
 
 const __dirname = import.meta.dirname;
 const PORT = process.env.PORT || 3000;
@@ -15,11 +15,38 @@ app.get('/', async (req, res) => {
 	res.render('index');
 })
 
-app.get('/categories', async (req, res)=>{
+app.get('/categories', async (req, res) => {
 	const categories = await getAllCategory();
 	res.render('allcategories', { categories: categories });
+});
+
+app.get('/products', async (req, res) => {
+	const products = await getAllProduct();
+	res.render('allproducts', { products: products })
+});
+
+app.get('/products/:id', async (req, res)=>{
+	const { id } = req.params;
+	const product = await getSingleProduct(Number.parseInt(id));
+	if (!product)
+	{
+		res.redirect('/');
+		return;
+	}
+	res.send(product);
 })
 
-app.listen(PORT, async ()=>{
-	console.log(`App listenig in port ${PORT}`);
+app.get('/categories/:id', async (req, res)=>{
+	const { id } = req.params;
+	const category = await getSingleCategory(Number.parseInt(id));
+	if (!category)
+	{
+		res.redirect('/');
+		return;
+	}
+	res.send(category);
+})
+
+app.listen(PORT, async () => {
+	console.log(`App is running in port ${PORT}`);
 });
