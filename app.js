0ada937c77abf06f29bd590ec126c626c1965324
Jormaedes Luís <jormahed@gmail.com>
2026-08-11 16:27:21 +0100

    feat: implement all categories view

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import process from 'process';
 import path from 'path';
-import pool from './db/db.js';
-
+import { getAllCategory } from './db/queires.js';
 
 const __dirname = import.meta.dirname;
 const PORT = process.env.PORT || 3000;
@@ -10,9 +9,15 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-app.get('/', (req, res)=> {
-	res.send('<h1>Inventory App</h1>');
+app.get('/', async (req, res) => {
+	res.render('index');
+})
+
+app.get('/categories', async (req, res)=>{
+	const categories = await getAllCategory();
+	res.render('allcategories', { categories: categories });
 })
 
 app.listen(PORT, async ()=>{
