7dd236502069b95eb2d4198ff206769fd755f21e
Jormaedes Luís <jormahed@gmail.com>
2026-08-12 12:29:37 +0100

    feat: add trycatch in queries

--- db/queires.js
+++ db/queires.js
@@ -1,50 +1,85 @@
 import pool from "./db.js";
 
 async function getAllCategory() {
+	try {
 	const query = "SELECT * FROM category ORDER BY id";
 	const { rows } = await pool.query(query);
 	return rows;
+	} catch (error) {
+		console.error("Error fetching categories:", error);
+		throw error;
+	}
 }
 
 async function getAllProduct() {
+	try {
 	const query = "SELECT product.id, product.name, qtd_stock, price, category.name as category_name FROM product JOIN category ON product.category_id = category.id";
 	const { rows } = await pool.query(query);
 	return rows;
+	} catch (error) {
+		console.error("Error fetching products:", error);
+		throw error;
+	}
 }
 
 async function getSingleProduct(id) {
-	const query = `
+	try {
+		const query = `
   SELECT product.id, product.name, qtd_stock, price, category.name AS category_name
   FROM product
   JOIN category ON product.category_id = category.id
   WHERE product.id = $1
 `;
-	const { rows } = await pool.query(query, [id]);
-	const product = rows[0];
-	return product;
+		const { rows } = await pool.query(query, [id]);
+		const product = rows[0];
+		return product;
+	} catch (error) {
+		console.error("Error fetching product:", error);
+		throw error;
+	}
 }
 
 async function getSingleCategory(id) {
-	const query = `SELECT * FROM category WHERE id = $1`;
-	const { rows } = await pool.query(query, [id]);
-	const category = rows[0];
-	return category;
+	try {
+		const query = "SELECT * FROM category WHERE id = $1";
+		const { rows } = await pool.query(query, [id]);
+		const category = rows[0];
+		return category;
+	} catch (error) {
+		console.error("Error fetching category:", error);
+		throw error;
+	}
 }
 
 async function insertCategory(category) {
-	const query = "INSERT INTO category(name, description) VALUES ($1, $2)";
-	await pool.query(query, [category.name, category.description]);
+	try {
+		const query = "INSERT INTO category(name, description) VALUES ($1, $2)";
+		await pool.query(query, [category.name, category.description]);
+	} catch (error) {
+		console.error("Error inserting category:", error);
+		throw error;
+	}
 }
 
 async function getCategoryNames() {
-	const query = "SELECT id, name FROM category";
-	const { rows } = await pool.query(query);
-	return rows;
+	try {
+		const query = "SELECT id, name FROM category";
+		const { rows } = await pool.query(query);
+		return rows;
+	} catch (error) {
+		console.error("Error fetching category names:", error);
+		throw error;
+	}
 }
 
 async function insertProduct(product) {
-	const query = "INSERT INTO product(name, qtd_stock, price, category_id) VALUES ($1, $2, $3, $4)";
-	await pool.query(query, [product.name, product.stock, product.price, product.category]);
+	try {
+		const query = "INSERT INTO product(name, qtd_stock, price, category_id) VALUES ($1, $2, $3, $4)";
+		await pool.query(query, [product.name, product.stock, product.price, product.category]);
+	} catch (error) {
+		console.error("Error inserting product:", error);
+		throw error;
+	}
 }
 
 async function updateCategory(id, category) {
@@ -57,14 +92,14 @@ async function deleteCategory(id) {
 		const query = "DELETE FROM category WHERE id = $1";
 		await pool.query(query, [id]);
 	} catch (error) {
-		throw new Error(`Error deleting category with id ${id}: ${error.message}`);	
+		throw new Error(`Error deleting category with id ${id}: ${error.message}`);
 	}
 }
 
 async function updateProduct(id, product) {
 	try {
-	const query = "UPDATE product SET name = $1, qtd_stock = $2, price = $3, category_id = $4 WHERE id = $5";
-	await pool.query(query, [product.name, product.stock, product.price, product.category, id]);
+		const query = "UPDATE product SET name = $1, qtd_stock = $2, price = $3, category_id = $4 WHERE id = $5";
+		await pool.query(query, [product.name, product.stock, product.price, product.category, id]);
 	} catch (error) {
 		console.error("Error updating product:", error);
 	}
